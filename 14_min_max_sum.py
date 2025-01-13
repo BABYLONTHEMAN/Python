8a25583b3e96b4cd31038fd508ef7a9895e9f90d
BABYLONTHEMAN <70661469+BABYLONTHEMAN@users.noreply.github.com>
2025-01-13 09:24:14 -0800

    test VS push

--- 14_min_max_sum.py
+++ 14_min_max_sum.py
@@ -1,14 +1,14 @@
 rectangles = [[5, 8], [3, 9], [5, 12], [16, 5]]
 def bnwsa(rectangles):
-    zor = 0
+    zoraka = 0
     for a ,b in rectangles:
         kam = min(a,b)          #'min' kamtrin la a,b agaretawa
-        zor = max(zor,kam)      #'max' zortrin la a,b agaretawa
+        zoraka = max(zoraka,kam)      #'max' zortrin la a,b agaretawa
         #sum(1 for a,b in rectangles)zhmaray indexakan abzhere #output 4
         #but sum(1 for a,b in rectangles if ...) # zhmara 3 anwse, chonka la if'aka tanha 3 dana yaksana 
-    bzhmera = sum(1 for a , b in rectangles if min(a,b) ==zor) #sum(1 for a,b in rectangles) zhmaray indexakan abzhera #4
+    bzhmera = sum(1 for a , b in rectangles if min(a,b) ==zoraka) #sum(1 for a,b in rectangles) zhmaray indexakan abzhera #4
     return bzhmera
-#calling function
+# change in VS push add terminal thingy test
 eshka = bnwsa(rectangles)
 print(eshka)
 
